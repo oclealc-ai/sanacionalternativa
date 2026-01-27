1d0af6b347734f129604c55a69790f43a07e1ee9
Ovidio César Leal Cavazos <ovidio.lealc@uanl.mx>
2026-01-26 18:16:42 -0600

    cambio en config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 # config.py
 
-from dotenv import load_dotenv
 import os
 
-# Carga variables de .env
-load_dotenv()
+if os.getenv("FLASK_ENV") != "production":
+    try:
+        from dotenv import load_dotenv
+        load_dotenv()
+    except ImportError:
+        pass  # dotenv no instalado (normal en producción)
 
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
@@ -12,17 +15,20 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
 SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = int(os.getenv("SMTP_PORT"))
+SMTP_PORT = int(os.getenv("SMTP_PORT")) if os.getenv("SMTP_PORT") else None
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 
 WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
 WHATSAPP_PHONE_ID = os.getenv("WHATSAPP_PHONE_ID")
-WHATSAPP_URL = f"https://graph.facebook.com/v22.0/{WHATSAPP_PHONE_ID}/messages"
+WHATSAPP_URL = (
+    f"https://graph.facebook.com/v22.0/{WHATSAPP_PHONE_ID}/messages"
+    if WHATSAPP_PHONE_ID
+    else None
+)
+
+TWILIO_SID   = os.getenv("TWILIO_SID")
+TWILIO_TOKEN = os.getenv("TWILIO_TOKEN")
+TWILIO_PHONE = os.getenv("TWILIO_PHONE")
 
 URL_BASE = os.getenv("URL_BASE")
-
-
-TWILIO_SID   = "AC04cdab72efeef8ead3cc091055de9139"
-TWILIO_TOKEN = "33a7183ef59eba6ab52f3e7ad5a489c5"
-TWILIO_PHONE = "+12185036702"
